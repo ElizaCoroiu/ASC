bf3c8552afaa4d83420f40fc66995d9d66de9629
Eliza Coroiu <coroiu.eliza31@yahoo.com>
2019-12-07 20:39:39 +0200

    Add BCD laboratory

---
+++ BCD/BCD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, digit;
+            string result = "";
+
+            input = int.Parse(Console.ReadLine());
+
+            while (input > 0)
+            {
+                digit = input % 10;
+                input /= 10;
+
+                result = Convert.ToString(digit, 2).PadLeft(4, '0') + " " + result;
+            }
+
+            Console.Write(result + " ");
+        }
+    }
+}
